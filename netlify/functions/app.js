3dc146b5dc460c4b6abaf20a247658e0884aa29c
spacehawk-dotcom <spacehawk@hanmail.net>
2026-07-22 12:14:37 +0900

    외기온도센서 교체

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -104,7 +104,7 @@ exports.handler = async (event, context) => {
         { id: 'bfb668aa0470a59280kzot', name: '육성_5배치', fbCol: 'grower',  fbBatch: 5 },
         { id: 'bff5798c4d866a911090af', name: '육성_6배치', fbCol: 'grower',  fbBatch: 6 },
         { id: 'bfba0d33b943894f3eddgh', name: '육성_7배치', fbCol: 'grower',  fbBatch: 7 },
-        { id: 'bf13f302e48b276482bzjb', name: '외부온도', fbCol: 'grower',  fbBatch: 8 }
+        { id: 'bf96609b8d76a4cdc0beff', name: '외부온도', fbCol: 'grower',  fbBatch: 8 }
 
     ];
 
